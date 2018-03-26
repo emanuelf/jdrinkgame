1176954abbdff5d2ce560853ece336f8e87cf594
Emanuel Henrique Fritsch <emanuelfritsch@outlook.com>
2018-03-26 11:40:39 -0300

    Priemiro commit.

---
+++ JDrinkGame/src/com/jdrinkgame/challenge/Destill.java
@@ -0,0 +1,16 @@
+package com.jdrinkgame.challenge;
+
+public enum Destill {
+	UMADOSE("Indique alguém para beber uma dose","Você pode escolher alguem para beber uma dose ou você mesmo pode beber um dose"),
+	DUASDOSE("Indique duas para beber uma dose cada","Você pode duas pessoas para cada uma delas beba uma dose"),
+	TRESDOSE("Indique três pessoas para beber uma dose cada","Você pode indicar três pessoa para que cada uma delas beba uma dose"),
+	VOCEBEBE("","");
+	
+	
+	private String desc;
+	
+	
+	Destill(String desc, String descComplete){
+		
+	}
+}
